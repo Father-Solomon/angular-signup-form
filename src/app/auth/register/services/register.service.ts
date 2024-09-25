e9a07e6d8d2034c1b7b244ef8973ada61a6a7d89
Anton Kucherenko <antonkycherenko@gmail.com>
2024-09-25 18:27:49 +0300

    dirty intermediate commit

---
+++ src/app/auth/register/services/register.service.ts
@@ -0,0 +1,61 @@
+import {computed, inject, Injectable, signal} from '@angular/core';
+import {AuthService} from "../../../shared/data-access/auth.service";
+import {catchError, EMPTY, Subject, switchMap} from "rxjs";
+import {ICredentials} from "../../../shared/interfaces/credentials.interface";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+
+export type RegisterStatus = 'pending' | 'creating' | 'success' | 'error';
+
+interface RegisterState {
+  status: RegisterStatus;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  private authService = inject(AuthService);
+
+  // sources
+  error$ = new Subject<any>();
+  createUser$ = new Subject<ICredentials>();
+
+  userCreated$ = this.createUser$.pipe(
+    switchMap((credentials) =>
+      this.authService.createAccount(credentials ).pipe(
+        catchError((err) => {
+          this.error$.next(err);
+          return EMPTY;
+        })
+      )
+    )
+  );
+
+  // state
+  private state = signal<RegisterState>({
+    status: 'pending',
+  });
+
+  // selectors
+  status = computed(() => this.state().status);
+  constructor() {
+    // reducers
+    this.userCreated$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'success' }))
+      );
+
+    this.createUser$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'creating' }))
+      );
+
+    this.error$
+      .pipe(takeUntilDestroyed())
+      .subscribe(() =>
+        this.state.update((state) => ({ ...state, status: 'error' }))
+      );
+  }
+}
--- src/app/auth/register/utils/password-matches.ts
+++ src/app/auth/register/utils/password-matches.ts
@@ -3,6 +3,6 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export const passwordMatchesValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const password = control.get('password')?.value;
   const confirmPassword = control.get('confirmPassword')?.value;
-
-  return password && confirmPassword && password === confirmPassword ? null : { passwordMatch: true };
+  console.log('passwordMatchesValidator', password && confirmPassword && password === confirmPassword ? null : { passwordMatch: true })
+  return password && confirmPassword && password === confirmPassword ? {passwordMatch: false} : { passwordMatch: true };
 };
